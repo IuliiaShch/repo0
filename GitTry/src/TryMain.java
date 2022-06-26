ce5547315e0ab066deae5e67c3c2152087a36294
IuliiaDev <iuliiashcher@gmail.com>
2022-06-26 18:25:08 +0300

    New commit

---
+++ GitTry/src/TryMain.java
@@ -0,0 +1,15 @@
+
+public class TryMain {
+	private int amount; 
+	
+	public TryMain() {
+		// TODO Auto-generated constructor stub
+		this.amount=0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
